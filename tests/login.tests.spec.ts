92307d87cbfc046d239e755616d2e829461000a8
yevhenii kotikov <yevheniikotikov@gmail.com>
2026-08-10 10:50:29 +0300

    login: negative test

--- tests/login.tests.spec.ts
+++ tests/login.tests.spec.ts
@@ -1,22 +1,42 @@
 import { test, expect } from "@fixtures/appFixture";
 
-import { standardUser } from "../framework/resources/Users";
 import URLs from "../framework/constants/pageUrls";
+import AppConstants from "../framework/constants/app";
+import { standardUser } from "../framework/resources/Users";
 import { MenuItems } from "../framework/enums/pages";
+import { UserAuthData } from "../framework/types/UserAuthData";
+import { generateAuthData } from "../framework/utils/generator.user.data";
 
 test.describe("Login tests", () => {
     test("Successful login, logout, re-login sequence", async ({
-        appNoUser
+        appNoUser,
+        baseURL
     }) => {
+        const expectedURL = new URL(URLs.Inventory, baseURL).href;
+
         await appNoUser.PAGES.login.navigate();
         await appNoUser.PAGES.login.loginAs(standardUser);
-        expect(appNoUser.PAGES.inventory.page.url()).toContain(URLs.Inventory);
+        expect(appNoUser.page.url()).toBe(expectedURL);
 
         await appNoUser.PAGES.topBar.openMenu();
         await appNoUser.PAGES.topBar.menu.select(MenuItems.Logout);
         expect(appNoUser.PAGES.login.loginForm).toBeVisible();
 
         await appNoUser.PAGES.login.loginAs(standardUser);
-        expect(appNoUser.PAGES.inventory.page.url()).toContain(URLs.Inventory);
+        expect(appNoUser.page.url()).toBe(expectedURL);
+    });
+
+    test("Failed login, incorrect password", async ({ appNoUser, baseURL }) => {
+        const userWithWrongPassword: UserAuthData = generateAuthData();
+        userWithWrongPassword.UserName = standardUser.UserName;
+
+        const expectedURL = new URL(String(baseURL)).href;
+
+        await appNoUser.PAGES.login.navigate();
+        await appNoUser.PAGES.login.loginAs(userWithWrongPassword);
+
+        expect(appNoUser.page.url()).toBe(expectedURL);
+        expect(appNoUser.PAGES.login.loginForm).toBeVisible();
+        expect(appNoUser.PAGES.login.error).toHaveText(AppConstants.auth_error);
     });
 });
